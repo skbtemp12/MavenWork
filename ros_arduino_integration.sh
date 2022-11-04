d95a29b5779f2d1a9594f8e53d2b91bf180d1769
skbtemp12 <73464906+skbtemp12@users.noreply.github.com>
2022-11-04 18:28:17 +0600

    Add files via upload

---
+++ ros_arduino_integration.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Author: Sheikh Shakib Hossain
+# Version: 0.1 ( Not Tested )
+
+
+# 1.1 Install rosserial and requirements
+sudo apt update;
+sudo apt install git ros-noetic-rosserial ros-noetic-rosserial-python ros-noetic-rosserial-arduino -y;
+
+# 1.2 Installing from Source onto the ROS workstation
+cd ~/catkin_ws/src
+git clone https://github.com/ros-drivers/rosserial.git
+
+cd .. # now we're inside ~/catkin_ws/
+catkin_make;
+catkin_make install;
+
+# Install ros_lib into the Arduino Environment 
+cd ~/Arduino/libraries/ # For arduino 1.8.6
+rm -rf ros_lib
+rosrun rosserial_arduino make_libraries.py .
+
+# Giving the port ACM0 read, write, executable power
+sudo usermod -a -G dialout $USER
+sudo chmod 777 /dev/ttyACM0
+
+exit
